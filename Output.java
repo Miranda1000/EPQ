7b7aedd6bb764028cf3e04ba6d11a755149e3ef8
Miranda1000 <72695638+Miranda1000@users.noreply.github.com>
2021-01-27 14:39:48 +0000

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,18 @@
+// How to output text in Java to the user
+// Source: https://www.w3schools.com/java/java_syntax.asp
+
+public class Output {
+	
+	public static void main (String args []) {
+		
+		// Output Hello World
+		
+		System.out.println("Hello world");
+		
+		
+		
+	}
+
+}
+
+// [2]
